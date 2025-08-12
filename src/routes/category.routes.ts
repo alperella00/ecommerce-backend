696cc9595969b048a55c6681e1fef98db2836cd2
alperella00 <alperella3101@gmail.com>
2025-08-12 18:28:56 +0300

    chore(backend/routes): category.routes tidy-up

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createCategory, listCategories, upsertCategorySchema } from "../controllers/category.controller";
+import { createCategory, listCategories, upsertCategorySchema, updateCategory, deleteCategory } from "../controllers/category.controller";
 import { validate } from "../middlewares/validate.middleware";
 import { requireAdmin, requireAuth } from "../middlewares/auth.middleware";
 
@@ -7,5 +7,7 @@ const r = Router();
 
 r.get("/", listCategories);
 r.post("/", requireAuth, requireAdmin, validate(upsertCategorySchema), createCategory);
+r.patch("/:id", requireAuth, requireAdmin, updateCategory);
+r.delete("/:id", requireAuth, requireAdmin, deleteCategory);
 
 export default r;
